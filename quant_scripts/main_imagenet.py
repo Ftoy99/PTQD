e6909a5bee4d075e67a3e286f83e06fd837fd77f
Ftoy <spit10000@yahoo.gr>
2025-03-20 11:45:02 +0200

    Import fix for only rank zero

--- quant_scripts/main_imagenet.py
+++ quant_scripts/main_imagenet.py
@@ -24,7 +24,7 @@ def load_model_from_config(config, ckpt):
     sd = pl_sd["state_dict"]
     model = instantiate_from_config(config.model)
     m, u = model.load_state_dict(sd, strict=False)
-    model.cuda()
+    model.to("cuda")
     model.eval()
     return model
 
