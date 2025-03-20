e470dbd1fc55dd4faa8e0dadb26b2ee0b8dbb78b
Ftoy <spit10000@yahoo.gr>
2025-03-20 09:48:17 +0200

    Import fix for only rank zero

--- ldm/models/diffusion/ddpm.py
+++ ldm/models/diffusion/ddpm.py
@@ -10,13 +10,13 @@ import torch
 import torch.nn as nn
 import numpy as np
 import pytorch_lightning as pl
+from lightning_fabric.utilities import rank_zero_only
 from torch.optim.lr_scheduler import LambdaLR
 from einops import rearrange, repeat
 from contextlib import contextmanager
 from functools import partial
 from tqdm import tqdm
 from torchvision.utils import make_grid
-from pytorch_lightning.utilities.distributed import rank_zero_only
 
 from ldm.util import log_txt_as_img, exists, default, ismap, isimage, mean_flat, count_params, instantiate_from_config
 from ldm.modules.ema import LitEma
