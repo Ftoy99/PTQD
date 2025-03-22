ce029f21a09cd30830286be02cdb58f826361696
Ftoy <spit10000@yahoo.gr>
2025-03-22 08:38:35 +0200

    Collect data

--- debug.py
+++ debug.py
@@ -3,5 +3,6 @@ import torch
 inputs_collected = torch.load("imagenet_input_20steps.pth")
 for x in inputs_collected:
     print(x[0].shape)
+    print(x[1])
     print(x[1].shape)
     print(x[2].shape)
