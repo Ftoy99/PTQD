9a82615fa08e24d930b407777c7441a1b3052853
Ftoy <spit10000@yahoo.gr>
2025-03-22 08:35:42 +0200

    Collect data

--- debug.py
+++ debug.py
@@ -1,4 +1,7 @@
 import torch
 
 inputs_collected = torch.load("imagenet_input_20steps.pth")
-print(inputs_collected[0])
+print(inputs_collected[0][0])
+print(inputs_collected[0][1])
+print(inputs_collected[0][2])
+print(inputs_collected[0][3])
