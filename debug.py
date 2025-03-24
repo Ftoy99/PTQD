07a0b660b144e17294e0f67c1f03f19b96f8e33b
Ftoy <spit10000@yahoo.gr>
2025-03-24 11:15:48 +0200

    Remove dependencies

--- debug.py
+++ debug.py
@@ -10,9 +10,9 @@ import torch
 
 inputs_collected = torch.load("data_error_t_w4a8_scale3.0_eta0.0_step20.pth")
 for x in inputs_collected:
-    print(x[100][0])
-    print(x[100][1])
-    print(x[100][2])
-    print(x[100][3])
-    print(x[100][4])
-    print(x[100][5])
+    print(x[0][0])
+    print(x[1][0])
+    print(x[2][0])
+    print(x[3][0])
+    print(x[4][0])
+    print(x[5][0])
