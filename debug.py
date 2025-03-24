0a50893c69e0c2efd898b1c4d01ea9d9db296ffb
Ftoy <spit10000@yahoo.gr>
2025-03-24 11:18:59 +0200

    Debug error collection

--- debug.py
+++ debug.py
@@ -10,9 +10,6 @@ import torch
 
 inputs_collected = torch.load("data_error_t_w4a8_scale3.0_eta0.0_step20.pth")
 for x in inputs_collected:
-    print(x[0])
-    print(x[1])
-    print(x[2])
-    print(x[3])
-    print(x[4])
-    print(x[5])
+    print(x[0].shape)
+    print(x[1].shape)
+    print(x[2].shape)
